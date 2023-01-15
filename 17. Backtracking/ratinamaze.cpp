89a317a6b63f4a591d6012fb56e22ace228c9ae0
Ishan Bhardwaj <theishanbh@gmail.com>
2023-01-15 22:59:40 +0530

    backtracking question done

---
+++ 17. Backtracking/ratinamaze.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isSafe(int **arr, int x, int y, int n)
+{
+    if (x < n && y < n && arr[x][y] == 1)
+        return true;
+    return false;
+}
+
+bool ratInMaze(int **arr, int x, int y, int n, int **solArr)
+{
+    if (x == n - 1 && y == n - 1)
+    {
+        solArr[x][y] = 1;
+        return true;
+    }
+    if (isSafe(arr, x, y, n))
+    {
+        solArr[x][y] = 1;
+        if (ratInMaze(arr, x + 1, y, n, solArr))
+        {
+            return true;
+        }
+        if (ratInMaze(arr, x, y + 1, n, solArr))
+        {
+            return true;
+        }
+        solArr[x][y] = 0;
+        return false;
+    }
+    return false;
+}
+
+int main()
+{
+    int n = 5;
+    int **arr = new int *[n];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = new int;
+    }
+    // input
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    // 1 0 1 0 1
+    // 1 1 1 1 1
+    // 0 1 0 1 0
+    // 1 0 0 1 1
+    // 1 1 1 0 1
+
+    // solution array
+    int **solArr = new int *[n];
+    for (int i = 0; i < n; i++)
+    {
+        solArr[i] = new int;
+        for (int j = 0; j < n; j++)
+        {
+            solArr[i][j] = 0;
+        }
+    }
+
+    if (ratInMaze(arr, 0, 0, n, solArr) == true)
+    {
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                cout << solArr[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+    else
+    {
+        cout << "No soln possible." << endl;
+    }
+
+    return 0;
+}
